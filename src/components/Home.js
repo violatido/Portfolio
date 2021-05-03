3440408745e264f5409209ed23b6d1b34437ee1e
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-05-03 14:51:52 -0400

    adds background image and welcome to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,17 @@
 import React from "react";
+import image from "../bkgd-img.jpg";
 
 export default function Home() {
-  return <h1>Home Page</h1>
+  return (
+    <main>
+      <img 
+        src={image} 
+        alt="background- orange/pink textured wallpaper" 
+        className="absolute object-cover w-full h-full" 
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hi! I'm Ilana</h1>
+      </section>
+    </main>
+  );
 }
