b608987640660b03e107c6d191a4ab8108da8478
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-05-03 14:22:57 -0400

    creates darker background for nav links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,19 +3,36 @@ import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <header>
-      <div>
-        <nav>
-          <NavLink to="/" exact>
-            Ilana
+    <header className="bg-red-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink 
+            to="/" 
+            exact 
+            activeClassName="text-white" 
+            className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+          >
+            Ilana Rose Mercer
           </NavLink>
-          <NavLink to="/post">
+          <NavLink 
+            to="/post"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
             Blog
           </NavLink>
-          <NavLink to="/project">
+          <NavLink 
+            to="/project"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
             Projects
           </NavLink>
-          <NavLink to="/about">
+          <NavLink 
+            to="/about"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
             About Me
           </NavLink>
         </nav>
