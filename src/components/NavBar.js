c34589b239ff922fbeb389fa27dda356dac1c060
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-05-03 14:12:53 -0400

    creates nav bar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-  return <h1>NavBar Page</h1>
+  return (
+    <header>
+      <div>
+        <nav>
+          <NavLink to="/" exact>
+            Ilana
+          </NavLink>
+          <NavLink to="/post">
+            Blog
+          </NavLink>
+          <NavLink to="/project">
+            Projects
+          </NavLink>
+          <NavLink to="/about">
+            About Me
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  );
 }
