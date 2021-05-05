5e980d9c5a7bd3dc50ac0d283c8a4c8016184ba3
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-05-05 12:53:27 -0400

    creates groq query for project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
 
 export default function Project() {
-  return <h1>Project Page</h1>
+  // query for data
+  const [projectData, setProjectData] = useState(null);
+
+  useEffect(() => {
+    sanityClient.fetch(`*[_type == "project]{
+      title,
+      date,
+      description,
+      projectType,
+      link,
+      tags
+    }`)
+    .then((data) => setProjectData(data))
+    .catch(console.error);
+  }, []);
+
+  return 
 }
