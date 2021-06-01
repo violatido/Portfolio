79d56738fdbc2b096e512694aa869be372a014fc
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-06-01 10:48:51 -0400

    Adds Block-Content package, fills in content and styling for single post

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import sanityClient from "../client.js";
 import imageUrlBuilder from "@sanity/image-url";
   // pulls out the url of an image from sanity studio
+import BlockContent from "@sanity/block-content-to-react"
 
 
 // creates building blocks of urlFor()
@@ -17,28 +18,66 @@ export default function SinglePost() {
   const { slug } = useParams();
 
   useEffect(() => {
-    // whatever slug clicked on will be viewes
-    sanityClient.fetch(`*[slug.current == "${slug}"]{ 
-      title,
-      _id,
-      slug,
-      mainImage{
-        asse->{
-          _id,
-          url
-        }
-      },
-      body,
-      "name": author->name, 
-      "authorImage": author->
-    }`
-    )
-    .then((data) => setSinglePost(data[0])) //grabs first piece of data from function 
-    .catch(console.error); 
+    // whatever slug clicked on will be viewed
+    sanityClient
+      .fetch(
+        `*[slug.current == "${slug}"]{ 
+            title,
+            _id,
+            slug,
+            mainImage{
+                asset->{
+                _id,
+                url
+              }
+            },
+            body,
+            "name": author->name, 
+            "authorImage": author->
+        }`
+      )
+      .then((data) => setSinglePost(data[0])) //grabs first piece of data from function 
+      .catch(console.error); 
   }, [slug]);
 
   if (!singlePost) return <div>Loading...</div>
 
-
-  return <h1>SinglePost Page</h1>
+  return (
+    <main className="bg-gray-200 min-h-screen p-12">
+      <article className="container shadow-lg mx-auto bg-green-100 rounded-lg">
+        <header className="relative">
+          <div className="absolute h-full w-full flex items-center justify-center p-8">
+            <div className="bg-white bg-opacity-75 rounded p-12">
+              <h1 className="cursive text-3xl lg:text-6l mb-4">
+                {singlePost.title}
+              </h1>
+              <div className="flex justify-center text-gray-800">
+                <img 
+                  src={urlFor(singlePost.authorImage).url()}
+                  alt={singlePost.name}
+                  className="w-10 h-10 rounded-full"
+                />
+                <p className="cursive flex items-center pl-2 text-2xl">
+                  {singlePost.name}
+                </p>
+              </div>              
+            </div>
+          </div>
+          <img 
+            src={singlePost.mainImage.asset.url} 
+            alt={singlePost.title} 
+            className="w-full object-cover rounded-t"
+            style={{ height: "400px" }}
+          />
+        </header>
+        <div className="px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full">
+          <BlockContent 
+            blocks={singlePost.body} 
+            projectId="8xnuhsze" 
+            dataset="production"
+          />
+        </div>
+      </article>
+    </main>
+  );
 }
